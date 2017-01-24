a361b8221f0d3b2cef82e0abcc99122426ca9c81
Arnas Dundulis <arnas@vinted.com>
2017-01-24 18:32:55 +0200

    Add ios target for carthage support

---
+++ MTDates-iOS/MTDates.h
@@ -0,0 +1,19 @@
+//
+//  MTDates-iOS.h
+//  MTDates-iOS
+//
+//  Created by Arnas Dundulis on 1/24/17.
+//  Copyright © 2017 Mysterious Trousers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTDates-iOS.
+FOUNDATION_EXPORT double MTDates_iOSVersionNumber;
+
+//! Project version string for MTDates-iOS.
+FOUNDATION_EXPORT const unsigned char MTDates_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTDates_iOS/PublicHeader.h>
+#import <MTDates/NSDate+MTDates.h>
+#import <MTDates/NSDateComponents+MTDates.h>
